7b5313034a2121c3147ff638c10c71e0add55064
Jeff Hammond <jeff.science@gmail.com>
2016-05-29 15:21:43 -0700

    try to resolve GASNet smp conduit issues on Mac

--- travis/install-berkeley-upc.sh
+++ travis/install-berkeley-upc.sh
@@ -13,14 +13,14 @@ MPI_IMPL=mpich
 #fi
 BUPC_PREFIX=$TRAVIS_ROOT/bupc-$CC
 
-export BUPC_RELEASE=berkeley_upc-2.22.0
+export BUPC_RELEASE=berkeley_upc-2.22.3
 
 # On Mac (not Linux), we see this error:
 #   upcrun: When network=smp compile with '-pthreads' or PSHM support to run with > 1 thread
 os=`uname`
 case $os in
     Darwin)
-        BUPC_NO_PTHREADS="--disable-par" # --enable-pshm # configure test fails
+        BUPC_NO_PTHREADS="" #--disable-par" # --enable-pshm # configure test fails
         MPI_ROOT=/usr/local
         ;;
     Linux)
@@ -29,36 +29,38 @@ case $os in
         ;;
 esac
 
+BUPC_NO_HPC_NETWORKS="--disable-auto-conduit-detect --disable-ibv --disable-gemini --disable-aries --disable-shmem --disable-pami  --disable-fca --disable-portals4 --disable-mxm --disable-hugetlbfs"
+BUPC_ONLY_X86_CPU="--disable-ppc64-probe --disable-ultrasparc-probe --disable-arch-sgi-ip27 --disable-tune-ppc970"
+BUPC_VM_FRIENDLY="--enable-force-yield-membars --disable-aligned-segments"
+
 if [ ! -d "$BUPC_PREFIX" ]; then
     wget --no-check-certificate -q http://upc.lbl.gov/download/release/$BUPC_RELEASE.tar.gz
     tar -xzf $BUPC_RELEASE.tar.gz
     cd $BUPC_RELEASE
     mkdir build && cd build
-    # disable IBV just in case Linux has headers for it
     case "$GASNET_CONDUIT" in
         smp)
-            ../configure --prefix=$BUPC_PREFIX --disable-aligned-segments $BUPC_NO_PTHREADS \
-                         --enable-$GASNET_CONDUIT --disable-auto-conduit-detect
+            ../configure --prefix=$BUPC_PREFIX $BUPC_NO_PTHREADS --enable-$GASNET_CONDUIT \
+                         $BUPC_NO_HPC_NETWORKS $BUPC_ONLY_X86_CPU $BUPC_VM_FRIENDLY
             ;;
         udp)
-            ../configure --prefix=$BUPC_PREFIX --disable-aligned-segments $BUPC_NO_PTHREADS \
-                         --enable-$GASNET_CONDUIT --disable-auto-conduit-detect
+            ../configure --prefix=$BUPC_PREFIX $BUPC_NO_PTHREADS --enable-$GASNET_CONDUIT \
+                         $BUPC_NO_HPC_NETWORKS $BUPC_ONLY_X86_CPU $BUPC_VM_FRIENDLY
             ;;
         ofi)
             # TODO factor Hydra out of Sandia OpenSHMEM install so it can be used as spawner here
-            ../configure --prefix=$BUPC_PREFIX --disable-aligned-segments $BUPC_NO_PTHREADS \
-                         --enable-$GASNET_CONDUIT --with-ofihome=$TRAVIS_ROOT/libfabric \
-                         --with-ofi-spawner=pmi --with-pmi=$TRAVIS_ROOT/hydra \
-                         --disable-auto-conduit-detect
+            ../configure --prefix=$BUPC_PREFIX $BUPC_NO_PTHREADS --enable-$GASNET_CONDUIT \
+                         --with-ofihome=$TRAVIS_ROOT/libfabric --with-ofi-spawner=pmi --with-pmi=$TRAVIS_ROOT/hydra \
+                         $BUPC_NO_HPC_NETWORKS $BUPC_ONLY_X86_CPU $BUPC_VM_FRIENDLY
             ;;
         mpi)
-            ../configure --prefix=$BUPC_PREFIX --disable-aligned-segments $BUPC_NO_PTHREADS \
-                         --enable-$GASNET_CONDUIT --with-mpi-cc=$MPI_ROOT/bin/mpicc \
-                         --disable-auto-conduit-detect
+            ../configure --prefix=$BUPC_PREFIX $BUPC_NO_PTHREADS --enable-$GASNET_CONDUIT \
+                         --with-mpi-cc=$MPI_ROOT/bin/mpicc \
+                         $BUPC_NO_HPC_NETWORKS $BUPC_ONLY_X86_CPU $BUPC_VM_FRIENDLY
             ;;
         *)
             echo "GASNet conduit not specified - configure will guess."
-            ../configure --prefix=$BUPC_PREFIX --disable-aligned-segments
+            ../configure --prefix=$BUPC_PREFIX $BUPC_ONLY_X86_CPU $BUPC_VM_FRIENDLY
             ;;
     esac
     make -j2
